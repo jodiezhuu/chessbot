c6cc9f4fe7ca19dc1624c3664cffd1a0d0dcbc08
jodiezhuu <jodiezhu12@gmail.com>
2022-11-29 16:49:30 -0500

    update piecelist functions

--- piececolor.h
+++ piececolor.h
@@ -1,3 +1,6 @@
 #ifndef __PIECECOLOR_H__
 #define __PIECECOLOR_H__
+
+enum class PieceColor { White, Black };
+
 #endif
--- piecelist.cc
+++ piecelist.cc
@@ -1,1 +1,41 @@
 #include "piecelist.h"
+#include "piece.h"
+#include "piececolor.h"
+#include <vector>
+#include <map>
+
+PieceList::PieceList(PieceColor color)
+: color{color} {}
+
+int PieceList::getPieceCount(Piece *piece) {
+    return pieceCount[piece];
+}
+
+std::vector<Piece *> *PieceList::getPieces() {
+    return &pieces;
+}
+
+void PieceList::addPiece(Piece *p) {
+    pieceCount[p] += 1;
+    pieces.emplace_back(p);
+}
+
+bool PieceList::removePieces(Piece *piece) {
+    int counter = 0;
+    if (pieceCount[piece] > 0) {
+        for (auto p: pieces) {
+            if (p == piece) {
+                break;
+            }
+            counter++;
+        }
+        pieces.erase(pieces.begin() + counter);
+        return true;
+    } return false;
+}
+
+PieceList::~PieceList() {
+    for (auto p: pieces) {
+        delete p;
+    }
+}
