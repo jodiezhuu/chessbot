3334273b4ba3ea6e3f947a1641e356589281de9f
jodiezhuu <jodiezhu12@gmail.com>
2022-11-29 17:17:34 -0500

    getter and setter for piece

--- piece.cc
+++ piece.cc
@@ -1,3 +1,15 @@
 #include "piece.h"
 #include "piecelist.h"
 #include "square.h"
+
+PieceType Piece::getPieceType() {
+    return type;
+}
+
+PieceColor Piece::getColor() {
+    return color;
+}
+
+Square *Piece::getPosition() {
+    return pos;
+}
--- piece.h
+++ piece.h
@@ -3,12 +3,16 @@
 #include "piececolor.h"
 #include "square.h"
 #include "piecetype.h"
+#include "board.h"
 class Piece {
     PieceColor color;
     Square *pos;
     PieceType type;
 public:
     Piece(PieceColor color, Square *pos, PieceType type);
+    PieceType getPieceType();
+    PieceColor getColor();
+    Square *getPosition();
 };
 
 #endif
