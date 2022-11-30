743a4da1d97d6fcd42589b8152fd148f75bfd276
jodiezhuu <jodiezhu12@gmail.com>
2022-11-30 01:48:37 -0500

    clean up code

--- Pieces/pawn.cc
+++ Pieces/pawn.cc
@@ -16,53 +16,62 @@ Square ** Pawn::validMoves() {
     int row = pos->getRow();
     int col = pos->getCol();
     Square ** moves;
-    if (color == PieceColor::Black && (b->getCell(row + 1, col))->getPiece() == nullptr && row + 1 <= 8) {
-        Square * m = b->getCell(row + 1, col);
-        moves[index] = m;
-        index++;
-        moveNum++;
-    }
-    if (color == PieceColor::White && (b->getCell(row - 1, col))->getPiece() == nullptr && row - 1 >= 1) {
-        Square * m = b->getCell(row - 1, col);
-        moves[index] = m;
-        index++;
-        moveNum++;
-    }
-    if (color == PieceColor::Black && (b->getCell(row + 1, col - 1))->getPiece()->getColor() != color && row + 1 <= 8 && col - 1 >= 1) {
-        Square * m = b->getCell(row + 1, col - 1);
-        moves[index] = m;
-        index++;
-        moveNum++;
-    } 
-    if (color == PieceColor::Black && (b->getCell(row + 1, col + 1))->getPiece()->getColor() != color && row + 1 <= 8 && col + 1 <= 8) {
-        Square * m = b->getCell(row + 1, col + 1);
-        moves[index] = m;
-        index++;
-        moveNum++;
-    }
-    if (color == PieceColor::White && (b->getCell(row - 1, col + 1))->getPiece()->getColor() != color && row - 1 >= 1 && col + 1 <= 8) {
-        Square * m = b->getCell(row - 1, col + 1);
-        moves[index] = m;
-        index++;
-        moveNum++;
-    }
-    if (color == PieceColor::White && (b->getCell(row - 1, col - 1))->getPiece()->getColor() != color && row - 1 >= 1 && col - 1 >= 1) {
-        Square * m = b->getCell(row - 1, col - 1);
-        moves[index] = m;
-        index++;
-        moveNum++;
-    }
-    if (color == PieceColor::Black && row == 2 && (b->getCell(row + 2, col))->getPiece() == nullptr) {
-        Square * m = b->getCell(row + 2, col);
-        moves[index] = m;
-        index++;
-        moveNum++;
-    }
-    if (color == PieceColor::White && row == 7 && (b->getCell(row - 2, col))->getPiece() == nullptr) {
-        Square * m = b->getCell(row - 2, col);
-        moves[index] = m;
-        index++;
-        moveNum++;
+    if (color == PieceColor::Black) {
+        if ((b->getCell(row + 1, col))->getPiece() == nullptr && row + 1 <= 8) {
+            Square * m = b->getCell(row + 1, col);
+            moves[index] = m;
+            index++;
+            moveNum++;
+        }
+        if ((b->getCell(row + 1, col - 1))->getPiece() != nullptr && (b->getCell(row + 1, col - 1))->getPiece()->getColor() != color && row + 1 <= 8 && col - 1 >= 1) {
+            Square * m = b->getCell(row + 1, col - 1);
+            moves[index] = m;
+            index++;
+            moveNum++;
+        }
+        if ((b->getCell(row + 1, col + 1))->getPiece() != nullptr && (b->getCell(row + 1, col + 1))->getPiece()->getColor() != color && row + 1 <= 8 && col + 1 <= 8) {
+            Square * m = b->getCell(row + 1, col + 1);
+            moves[index] = m;
+            index++;
+            moveNum++;
+        }
+        if ((b->getCell(row + 1, col + 1))->getPiece() != nullptr && (b->getCell(row + 1, col + 1))->getPiece()->getColor() != color && row + 1 <= 8 && col + 1 <= 8) {
+            Square * m = b->getCell(row + 1, col + 1);
+            moves[index] = m;
+            index++;
+            moveNum++;
+        }
+        if (row == 2 && (b->getCell(row + 2, col))->getPiece() == nullptr) {
+            Square * m = b->getCell(row + 2, col);
+            moves[index] = m;
+            index++;
+            moveNum++;
+        }
+    } else if (color == PieceColor::White) {
+        if ((b->getCell(row - 1, col))->getPiece() == nullptr && row - 1 >= 1) {
+            Square * m = b->getCell(row - 1, col);
+            moves[index] = m;
+            index++;
+            moveNum++;
+        }
+        if ((b->getCell(row - 1, col + 1))->getPiece() != nullptr && (b->getCell(row - 1, col + 1))->getPiece()->getColor() != color && row - 1 >= 1 && col + 1 <= 8) {
+            Square * m = b->getCell(row - 1, col + 1);
+            moves[index] = m;
+            index++;
+            moveNum++;
+        }
+        if ((b->getCell(row - 1, col - 1))->getPiece() != nullptr && (b->getCell(row - 1, col - 1))->getPiece()->getColor() != color && row - 1 >= 1 && col - 1 >= 1) {
+            Square * m = b->getCell(row - 1, col - 1);
+            moves[index] = m;
+            index++;
+            moveNum++;
+        }
+        if (row == 7 && (b->getCell(row - 2, col))->getPiece() == nullptr) {
+            Square * m = b->getCell(row - 2, col);
+            moves[index] = m;
+            index++;
+            moveNum++;
+        }
     }
     return moves;
 }
@@ -72,25 +81,28 @@ Square ** Pawn::capturingMoves() {
     int row = pos->getRow();
     int col = pos->getCol();
     Square ** captures;
-    if (color == PieceColor::Black && (b->getCell(row + 1, col - 1))->getPiece()->getColor() != color && row + 1 <= 8 && col - 1 >= 1) {
-        Square * m = b->getCell(row + 1, col - 1);
-        captures[index] = m;
-        index++;
-    } 
-    if (color == PieceColor::Black && (b->getCell(row + 1, col + 1))->getPiece()->getColor() != color && row + 1 <= 8 && col + 1 <= 8) {
-        Square * m = b->getCell(row + 1, col + 1);
-        captures[index] = m;
-        index++;
-    }
-    if (color == PieceColor::White && (b->getCell(row - 1, col + 1))->getPiece()->getColor() != color && row - 1 >= 1 && col + 1 <= 8) {
-        Square * m = b->getCell(row - 1, col + 1);
-        captures[index] = m;
-        index++;
-    }
-    if (color == PieceColor::White && (b->getCell(row - 1, col - 1))->getPiece()->getColor() != color && row - 1 >= 1 && col - 1 >= 1) {
-        Square * m = b->getCell(row - 1, col - 1);
-        captures[index] = m;
-        index++;
+    if (color == PieceColor::Black) {
+        if ((b->getCell(row + 1, col - 1))->getPiece() != nullptr && (b->getCell(row + 1, col - 1))->getPiece()->getColor() != color && row + 1 <= 8 && col - 1 >= 1) {
+            Square * m = b->getCell(row + 1, col - 1);
+            captures[index] = m;
+            index++;
+        } 
+        if ((b->getCell(row + 1, col + 1))->getPiece() != nullptr && (b->getCell(row + 1, col + 1))->getPiece()->getColor() != color && row + 1 <= 8 && col + 1 <= 8) {
+            Square * m = b->getCell(row + 1, col + 1);
+            captures[index] = m;
+            index++;
+        }
+    } else if (color == PieceColor::White) {
+        if ((b->getCell(row - 1, col + 1))->getPiece() != nullptr && (b->getCell(row - 1, col + 1))->getPiece()->getColor() != color && row - 1 >= 1 && col + 1 <= 8) {
+            Square * m = b->getCell(row - 1, col + 1);
+            captures[index] = m;
+            index++;
+        }
+        if ((b->getCell(row - 1, col - 1))->getPiece() != nullptr && (b->getCell(row - 1, col - 1))->getPiece()->getColor() != color && row - 1 >= 1 && col - 1 >= 1) {
+            Square * m = b->getCell(row - 1, col - 1);
+            captures[index] = m;
+            index++;
+        }
     }
     return captures;
 }
