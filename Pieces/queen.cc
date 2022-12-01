8e3bbfef9f2913d05dec4e0bf9e5009674db5e26
Jonath4n17 <59178400+Jonath4n17@users.noreply.github.com>
2022-12-01 00:42:27 -0500

    Queen logic

--- Pieces/queen.cc
+++ Pieces/queen.cc
@@ -2,3 +2,130 @@
 
 Queen::Queen(Board *b, PieceColor color, Square *pos, PieceType type)
 : Piece{b, color, pos, type} {}
+
+void Queen::calculateMoves() {
+    validMoves.clear();
+    capturingMoves.clear();
+    // right
+    for (int col = pos->getCol() + 1; col <= 7; ++col) {
+        Square * sq = b->getCell(pos->getRow(), col);
+        Piece *piece = sq->getPiece();
+        if (piece == nullptr) {
+            validMoves.emplace_back(sq);
+        } else if (piece->getColor() != color) {
+            validMoves.emplace_back(sq);
+            capturingMoves.emplace_back(sq);
+            break;
+        }
+    }
+    // left
+    for (int col = pos->getCol() - 1; col >= 0; --col) {
+        Square * sq = b->getCell(pos->getRow(), col);
+        Piece *piece = sq->getPiece();
+        if (piece == nullptr) {
+            validMoves.emplace_back(sq);
+        } else if (piece->getColor() != color) {
+            validMoves.emplace_back(sq);
+            capturingMoves.emplace_back(sq);
+            break;
+        }
+    }
+    // up
+    for (int row = pos->getRow() - 1; row >= 0; --row) {
+        Square * sq = b->getCell(row, pos->getCol());
+        Piece *piece = sq->getPiece();
+        if (piece == nullptr) {
+            validMoves.emplace_back(sq);
+        } else if (piece->getColor() != color) {
+            validMoves.emplace_back(sq);
+            capturingMoves.emplace_back(sq);
+            break;
+        }
+    }
+    // down
+    for (int row = pos->getRow() + 1; row <= 7; ++row) {
+    Square * sq = b->getCell(row, pos->getCol());
+    Piece *piece = sq->getPiece();
+    if (piece == nullptr) {
+        validMoves.emplace_back(sq);
+    } else if (piece->getColor() != color) {
+        validMoves.emplace_back(sq);
+        capturingMoves.emplace_back(sq);
+        break;
+    }
+    }
+    // left-up diagonal
+    for (int row = pos->getRow() - 1; row >= 0; --row) {
+        for (int col = pos->getCol() - 1; col >= 0; --col) {
+            Square * sq = b->getCell(row, col);
+            Piece *piece = sq->getPiece();
+            if (piece == nullptr) {
+                validMoves.emplace_back(sq);
+            } else if (piece->getColor() != color) {
+                validMoves.emplace_back(sq);
+                capturingMoves.emplace_back(sq);
+                break;
+            }
+        }
+    }
+    // right-up diagonal
+    for (int row = pos->getRow() - 1; row >= 0; --row) {
+        for (int col = pos->getCol() + 1; col <= 7; ++col) {
+            Square * sq = b->getCell(row, col);
+            Piece *piece = sq->getPiece();
+            if (piece == nullptr) {
+                validMoves.emplace_back(sq);
+            } else if (piece->getColor() != color) {
+                validMoves.emplace_back(sq);
+                capturingMoves.emplace_back(sq);
+                break;
+            }
+        }
+    }
+    // left-down diagonal
+    for (int row = pos->getRow() + 1; row <= 7; ++row) {
+        for (int col = pos->getCol() - 1; col >= 0; --col) {
+            Square * sq = b->getCell(row, col);
+            Piece *piece = sq->getPiece();
+            if (piece == nullptr) {
+                validMoves.emplace_back(sq);
+            } else if (piece->getColor() != color) {
+                validMoves.emplace_back(sq);
+                capturingMoves.emplace_back(sq);
+                break;
+            }
+        }
+    }
+    // right-down diagonal
+    for (int row = pos->getRow() + 1; row <= 7; ++row) {
+        for (int col = pos->getCol() + 1; col <= 7; ++col) {
+            Square * sq = b->getCell(row, col);
+            Piece *piece = sq->getPiece();
+            if (piece == nullptr) {
+                validMoves.emplace_back(sq);
+            } else if (piece->getColor() != color) {
+                validMoves.emplace_back(sq);
+                capturingMoves.emplace_back(sq);
+                break;
+            }
+        }
+    }
+}
+
+bool Queen::canBeCaptured() {
+    std::vector<Piece *> *list;
+    if (color == PieceColor::White) {
+        list = b->getBlackPieces()->getPieces();
+    } else {
+        list = b->getWhitePieces()->getPieces();
+    }
+
+    for (auto piece : *list) {
+        for (auto moves : piece->getCapturingMoves()) {
+            if (moves == pos) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
